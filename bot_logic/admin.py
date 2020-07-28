f42fd2f6dc5d30eac1a3c9894d7c96fce4ce8ce7
Дмитрий Брезгунов <dmitry.brezgunov@yandex.ru>
2020-07-28 23:44:32 +0300

    Исправления и расширение моделей

--- bot_logic/admin.py
+++ bot_logic/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from .models import Problem, Test, Hint, Restriction
+
+from .models import (Contest, Hint, Problem, Restriction, Sprint, Test, User,
+                     UserTestPair)
+
+
+class SprintAdmin(admin.ModelAdmin):
+    list_display = ('sprint_number', 'sprint_title', 'faculty', )
+    search_fields = ('number', 'title', )
+
+
+class ContestAdmin(admin.ModelAdmin):
+    list_display = ('contest_number', 'contest_title', 'sprint_number', )
+    search_fields = ('contest_number', 'contest_title', )
+    list_filter = ('sprint_number', )
 
 
 class ProblemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'sprint_number', 'contest_number')
-    search_fields = ('sprint_number', 'title')
-    list_filter = ('sprint_number', 'contest_number', 'title')
+    list_display = ('title', 'full_title', 'sprint_number',
+                    'contest_number', 'test_limit', )
+    search_fields = ('sprint_number', 'title',
+                     'full_title', 'contest_number', )
+    list_filter = ('sprint_number', 'contest_number', )
 
 
 class TestAdmin(admin.ModelAdmin):
@@ -16,14 +31,29 @@ class TestAdmin(admin.ModelAdmin):
 class HintAdmin(admin.ModelAdmin):
     list_display = ('text', 'problem')
     search_fields = ('problem', 'text')
+    list_filter = ('problem', )
 
 
 class RestrictionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'problem', 'counter')
-    search_fields = ('user',)
+    list_display = ('user', 'problem', 'request_counter', )
+    search_fields = ('user', 'problem', )
 
 
+class UserTestPairAdmin(admin.ModelAdmin):
+    list_display = ('user', 'test', )
+    search_fields = ('user', 'test', )
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('slack_id', 'first_name', 'last_name', 'cohort', )
+    search_fields = ('first_name', 'last_name', 'slack_id', )
+
+
+admin.site.register(Sprint, SprintAdmin)
+admin.site.register(Contest, ContestAdmin)
 admin.site.register(Problem, ProblemAdmin)
 admin.site.register(Test, TestAdmin)
 admin.site.register(Hint, HintAdmin)
 admin.site.register(Restriction, RestrictionAdmin)
+admin.site.register(UserTestPair, UserTestPairAdmin)
+admin.site.register(User, UserAdmin)
--- bot_logic/models.py
+++ bot_logic/models.py
@@ -1,48 +1,100 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth import get_user_model
 
 
-User = get_user_model()
+class User(AbstractUser):
+    slack_id = models.PositiveSmallIntegerField(
+        unique=True, verbose_name='id в Слаке', blank=True, null=True)
+    cohort = models.PositiveSmallIntegerField(
+        blank=True, null=True, verbose_name='Когорта')
+
+    def __str__(self):
+        return f'{self.get_full_name()}, когорта {self.cohort}'
+
+
+class Sprint(models.Model):
+    faculty = models.CharField(
+        max_length=255, verbose_name='Факультет',
+        blank=True, null=True)
+    sprint_number = models.PositiveSmallIntegerField(
+        verbose_name='Номер спринта')
+    sprint_title = models.CharField(
+        max_length=255, verbose_name='Название спринта')
+
+    class Meta:
+        verbose_name = 'Спринт'
+        verbose_name_plural = 'Спринты'
+
+    def __str__(self):
+        return f'{str(self.sprint_number)}. {self.sprint_title}'
+
+
+class Contest(models.Model):
+    contest_number = models.PositiveSmallIntegerField(
+        verbose_name='Номер контеста')  # unqiue=True?
+    contest_title = models.CharField(
+        max_length=255, verbose_name='Название контеста')
+    sprint_number = models.ForeignKey(  # возможно manytomany
+        Sprint, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='contest', verbose_name='Спринт')
+
+    class Meta:
+        verbose_name = "Контест"
+        verbose_name_plural = "Контесты"
+
+    def __str__(self):
+        return f'{str(self.contest_number)}. {self.contest_title}'
 
 
 class Problem(models.Model):
-    sprint_number = models.CharField(
-        max_length=255, verbose_name='Номер спринта')
-    contest_number = models.CharField(
-        max_length=255, verbose_name='Номер контеста')
+    sprint_number = models.ForeignKey(
+        Sprint, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='problem', verbose_name='Спринт')
+    contest_number = models.ForeignKey(
+        Contest, on_delete=models.CASCADE,
+        related_name='problem', verbose_name='Контест')
     title = models.CharField(max_length=3, verbose_name='Название задачи')
+    full_title = models.CharField(
+        max_length=255, verbose_name='Полное название')
+    test_limit = models.PositiveSmallIntegerField(
+        default=3, verbose_name='Лимит тестов')
 
     class Meta:
-        verbose_name = "Задача"
-        verbose_name_plural = "Задачи"
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
 
     def __str__(self):
-        return f'Спринт: {self.sprint_number}, Контест: {self.contest_number}, Задача: {self.title}'
+        return (f'{self.sprint_number} '
+                f'{self.contest_number} '
+                f'{self.title}. '
+                f'{self.full_title}')
 
 
 class Test(models.Model):
     problem = models.ForeignKey(
-        Problem, on_delete=models.CASCADE, related_name='test', verbose_name='Задача')
-    number = models.CharField(max_length=3, verbose_name='Номер теста')
+        Problem, on_delete=models.CASCADE,
+        related_name='test', verbose_name='Задача')
+    number = models.PositiveSmallIntegerField(verbose_name='Номер теста')
     test_file = models.FileField(
         upload_to='tests_files/')
 
     class Meta:
-        verbose_name = "Тест"
-        verbose_name_plural = "Тесты"
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
 
     def __str__(self):
-        return self.number
+        return f'{self.problem}. Тест №{str(self.number)}'
 
 
 class Hint(models.Model):
     problem = models.ForeignKey(
-        Problem, on_delete=models.CASCADE, related_name='hint', verbose_name='Задача')
+        Problem, on_delete=models.CASCADE,
+        related_name='hint', verbose_name='Задача')
     text = models.TextField(verbose_name='Текст подсказки')
 
     class Meta:
-        verbose_name = "Подсказка"
-        verbose_name_plural = "Подсказки"
+        verbose_name = 'Подсказка'
+        verbose_name_plural = 'Подсказки'
 
     def __str__(self):
         return self.text
@@ -50,15 +102,33 @@ class Hint(models.Model):
 
 class Restriction(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='test_counter', verbose_name='Студент')
+        User, on_delete=models.CASCADE,
+        related_name='restriction', verbose_name='Студент')
     problem = models.ForeignKey(
-        Problem, on_delete=models.CASCADE, related_name='test_counter', verbose_name='Задача')
-    counter = models.SmallIntegerField(
-        default=3, verbose_name='Количество оставшихся тестов')
+        Problem, on_delete=models.CASCADE,
+        related_name='restriction', verbose_name='Задача')
+    request_counter = models.PositiveSmallIntegerField(
+        default=0, verbose_name='Количество запросов на тесты')
 
-    class Meta:
-        verbose_name = "Ограничение"
-        verbose_name_plural = "Ограничения"
+    def check_request_limit(self, problem):
+        return problem.test_limit >= self.request_counter
 
     def __str__(self):
         return f'{self.user}, Задача: {self.problem}'
+
+    class Meta:
+        verbose_name = 'Ограничение'
+        verbose_name_plural = 'Ограничения'
+
+
+class UserTestPair(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='user_test_pair', verbose_name='Студент')
+    test = models.ForeignKey(
+        Test, on_delete=models.CASCADE,
+        related_name='user_test_pair', verbose_name='Тест')
+
+    class Meta:
+        verbose_name = 'Запрошенный студентом тест'
+        verbose_name_plural = 'Запрошенные студентами тесты'
