3aa4c581afefe6d58db330f118079a9d00e420d3
Igor Galitskiy <myseoru@gmail.com>
2020-08-07 21:01:16 +0300

    refactoring inInteractive

--- bot_logic/serviсes.py
+++ bot_logic/serviсes.py
@@ -1,8 +1,13 @@
 from django.conf import settings
-from .models import Hint, Test, Problem
+from .models import Hint, Test, Problem, User
 import os
 import json
 import requests
+import slack
+from .resources import *
+
+
+client = slack.WebClient(token=settings.SLACK_BOT_TOKEN)
 
 
 def slack_send_file(channel, text, value, **kwargs):
@@ -154,3 +159,99 @@ def choose_test_final(request):
     test = Test.objects.get(id=data['value'])
 
     return test, data['ts']
+
+
+def dialog_action(interactive_action):
+    '''Обработка диалоговой формы'''
+    submission = interactive_action["submission"]
+    channel = interactive_action["channel"]['id']
+    callback_id = interactive_action["callback_id"]
+    if callback_id == 'register_form':
+        name = submission['name']
+        email = submission['email']
+        kogort = submission['kogort']
+        # Пока так, потом надо переделать на id пользователя
+        if User.objects.filter(username=name):
+            client.chat_postMessage(
+                channel=channel, text='Маска, я тебя знаю!')
+        else:
+            new_user = User.objects.create(username=name, email=email)
+            new_user.save()
+            client.chat_postMessage(
+                channel=channel, text='Приятно познакомиться!')
+    if callback_id == 'test_form':
+        task = submission['task']
+        test = submission['test']
+    if Test.objects.filter(problem__title=task, number=test).exists():
+        result = Test.objects.get(problem__title=task, number=test)
+        title = 'Done!'
+        client.files_upload(
+            channels=channel, title=title, file=result.test_file.path)
+    else:
+        client.chat_postMessage(
+            channel=channel, text='У меня такого нет :(')
+    if callback_id == 'prompt_form':
+        task = submission['task']
+    if Hint.objects.filter(problem__title=task).exists():
+        prompt = Hint.objects.get(problem__title=task)
+        client.chat_postMessage(channel=channel, text=prompt.text)
+    else:
+        client.chat_postMessage(
+            channel=channel, text='У меня такого нет :(')
+
+
+def buttom_action(interactive_action, request):
+    '''Обработка нажатия кнопки'''
+    user = interactive_action["user"]["id"]
+    button = interactive_action["actions"][0].get("value")
+    block_id = interactive_action["actions"][0].get("block_id")
+    action_id = interactive_action["actions"][0].get("action_id")
+    selected_option = interactive_action[
+        "actions"][0].get("selected_option")
+    channel = interactive_action["channel"]['id']
+    ts = interactive_action["message"]["ts"]
+
+    if button == 'click_me_123':
+        '''У кнопок задаются уникальные значения для идентификации нажатия на определенной кнопке'''
+        client.chat_postMessage(
+            text='Котику понравилось!', channel=channel)
+
+    elif button == 'click_me_1234':
+        client.chat_postMessage(
+            channel=channel, blocks=build_sprint_block())
+
+    elif button == 'click_me_test':
+        print(interactive_action['trigger_id'])
+        client.dialog_open(trigger_id=interactive_action[
+                            'trigger_id'], dialog=json.dumps(choose_test))
+
+    elif button == 'click_me_hint':
+        client.dialog_open(trigger_id=interactive_action[
+                            'trigger_id'], dialog=json.dumps(choose_hint))
+
+    elif block_id == 'choose_sprint':
+        block = choose_contest(request)
+
+    elif block_id == 'choose_contest':
+        block = choose_task(request)
+
+    elif block_id == 'choose_task':
+        block = choose_test(request)
+
+    elif block_id == 'choose_test':
+        block = choose_test(request)
+
+    elif block_id == 'choose_test_final':
+        '''Обработка выбора селекта с тестами'''
+        test, ts = choose_test_final(request)
+        # Обновляем сообщение
+        client.chat_update(
+            channel=channel, ts=ts, blocks=test_section(test))
+
+    block_id_list = ['choose_sprint',
+                        'choose_contest', 'choose_task', 'choose_test']
+
+    if block_id in block_id_list:
+        client.chat_update(
+            channel=channel, ts=ts, blocks=block)    
+
--- bot_logic/views.py
+++ bot_logic/views.py
@@ -18,6 +18,7 @@ from .models import Hint, Test, Problem
 from .serviсes import *
 
 
+
 User = get_user_model()
 
 
@@ -73,97 +74,10 @@ class onInteractiv(View):
             pass
 
         elif interactive_action["type"] == "dialog_submission":
-            '''Обработка диалоговой формы'''
-            submission = interactive_action["submission"]
-            channel = interactive_action["channel"]['id']
-            callback_id = interactive_action["callback_id"]
-            if callback_id == 'register_form':
-                name = submission['name']
-                email = submission['email']
-                kogort = submission['kogort']
-                # Пока так, потом надо переделать на id пользователя
-                if User.objects.filter(username=name):
-                    client.chat_postMessage(
-                        channel=channel, text='Маска, я тебя знаю!')
-                else:
-                    new_user = User.objects.create(username=name, email=email)
-                    new_user.save()
-                    client.chat_postMessage(
-                        channel=channel, text='Приятно познакомиться!')
-            if callback_id == 'test_form':
-                task = submission['task']
-                test = submission['test']
-                if Test.objects.filter(problem__title=task, number=test).exists():
-                    result = Test.objects.get(problem__title=task, number=test)
-                    title = 'Done!'
-                    client.files_upload(
-                        channels=channel, title=title, file=result.test_file.path)
-                else:
-                    client.chat_postMessage(
-                        channel=channel, text='У меня такого нет :(')
-            if callback_id == 'prompt_form':
-                task = submission['task']
-                if Hint.objects.filter(problem__title=task).exists():
-                    prompt = Hint.objects.get(problem__title=task)
-                    client.chat_postMessage(channel=channel, text=prompt.text)
-                else:
-                    client.chat_postMessage(
-                        channel=channel, text='У меня такого нет :(')
+            dialog_action(interactive_action)
 
         elif interactive_action["type"] == "block_actions":
-            '''Обработка нажатия кнопки'''
-            user = interactive_action["user"]["id"]
-            button = interactive_action["actions"][0].get("value")
-            block_id = interactive_action["actions"][0].get("block_id")
-            action_id = interactive_action["actions"][0].get("action_id")
-            selected_option = interactive_action[
-                "actions"][0].get("selected_option")
-            channel = interactive_action["channel"]['id']
-            ts = interactive_action["message"]["ts"]
-
-            if button == 'click_me_123':
-                '''У кнопок задаются уникальные значения для идентификации нажатия на определенной кнопке'''
-                client.chat_postMessage(
-                    text='Котику понравилось!', channel=channel)
-
-            elif button == 'click_me_1234':
-                client.chat_postMessage(
-                    channel=channel, blocks=build_sprint_block())
-
-            elif button == 'click_me_test':
-                print(interactive_action['trigger_id'])
-                client.dialog_open(trigger_id=interactive_action[
-                                   'trigger_id'], dialog=json.dumps(choose_test))
-
-            elif button == 'click_me_hint':
-                client.dialog_open(trigger_id=interactive_action[
-                                   'trigger_id'], dialog=json.dumps(choose_hint))
-
-            elif block_id == 'choose_sprint':
-                block = choose_contest(request)
-
-            elif block_id == 'choose_contest':
-                block = choose_task(request)
-
-            elif block_id == 'choose_task':
-                block = choose_test(request)
-
-            elif block_id == 'choose_test':
-                block = choose_test(request)
-
-            elif block_id == 'choose_test_final':
-                '''Обработка выбора селекта с тестами'''
-                test, ts = choose_test_final(request)
-                # Обновляем сообщение
-                client.chat_update(
-                    channel=channel, ts=ts, blocks=test_section(test))
-
-            block_id_list = ['choose_sprint',
-                             'choose_contest', 'choose_task', 'choose_test']
-
-            if block_id in block_id_list:
-                client.chat_update(
-                    channel=channel, ts=ts, blocks=block)
+            buttom_action(interactive_action, request)
 
         '''Внимание!!! Нужно ответить пустым текстом, иначе будет ошибка'''
         return HttpResponse('', 200)
@@ -230,7 +144,102 @@ class Event(View):
 
         return HttpResponse("ok", 200)
 
-
 def write_json(data, filename='answer.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+
+def dialog_action(interactive_action):
+    '''Обработка диалоговой формы'''
+    submission = interactive_action["submission"]
+    channel = interactive_action["channel"]['id']
+    callback_id = interactive_action["callback_id"]
+    if callback_id == 'register_form':
+        name = submission['name']
+        email = submission['email']
+        kogort = submission['kogort']
+        # Пока так, потом надо переделать на id пользователя
+        if User.objects.filter(username=name):
+            client.chat_postMessage(
+                channel=channel, text='Маска, я тебя знаю!')
+        else:
+            new_user = User.objects.create(username=name, email=email)
+            new_user.save()
+            client.chat_postMessage(
+                channel=channel, text='Приятно познакомиться!')
+    if callback_id == 'test_form':
+        task = submission['task']
+        test = submission['test']
+    if Test.objects.filter(problem__title=task, number=test).exists():
+        result = Test.objects.get(problem__title=task, number=test)
+        title = 'Done!'
+        client.files_upload(
+            channels=channel, title=title, file=result.test_file.path)
+    else:
+        client.chat_postMessage(
+            channel=channel, text='У меня такого нет :(')
+    if callback_id == 'prompt_form':
+        task = submission['task']
+    if Hint.objects.filter(problem__title=task).exists():
+        prompt = Hint.objects.get(problem__title=task)
+        client.chat_postMessage(channel=channel, text=prompt.text)
+    else:
+        client.chat_postMessage(
+            channel=channel, text='У меня такого нет :(')
+
+
+def buttom_action(interactive_action, request):
+    '''Обработка нажатия кнопки'''
+    user = interactive_action["user"]["id"]
+    button = interactive_action["actions"][0].get("value")
+    block_id = interactive_action["actions"][0].get("block_id")
+    action_id = interactive_action["actions"][0].get("action_id")
+    selected_option = interactive_action[
+        "actions"][0].get("selected_option")
+    channel = interactive_action["channel"]['id']
+    ts = interactive_action["message"]["ts"]
+
+    if button == 'click_me_123':
+        '''У кнопок задаются уникальные значения для идентификации нажатия на определенной кнопке'''
+        client.chat_postMessage(
+            text='Котику понравилось!', channel=channel)
+
+    elif button == 'click_me_1234':
+        client.chat_postMessage(
+            channel=channel, blocks=build_sprint_block())
+
+    elif button == 'click_me_test':
+        print(interactive_action['trigger_id'])
+        client.dialog_open(trigger_id=interactive_action[
+                            'trigger_id'], dialog=json.dumps(choose_test))
+
+    elif button == 'click_me_hint':
+        client.dialog_open(trigger_id=interactive_action[
+                            'trigger_id'], dialog=json.dumps(choose_hint))
+
+    elif block_id == 'choose_sprint':
+        block = choose_contest(request)
+
+    elif block_id == 'choose_contest':
+        block = choose_task(request)
+
+    elif block_id == 'choose_task':
+        block = choose_test(request)
+
+    elif block_id == 'choose_test':
+        block = choose_test(request)
+
+    elif block_id == 'choose_test_final':
+        '''Обработка выбора селекта с тестами'''
+        test, ts = choose_test_final(request)
+        # Обновляем сообщение
+        client.chat_update(
+            channel=channel, ts=ts, blocks=test_section(test))
+
+    block_id_list = ['choose_sprint',
+                        'choose_contest', 'choose_task', 'choose_test']
+
+    if block_id in block_id_list:
+        client.chat_update(
+            channel=channel, ts=ts, blocks=block)    
